2ab3c2d394f26c47c1dafea8ef93c399a678a618
artyom.kuis <artyom.kuis@gmail.com>
2024-10-22 10:47:03 +0200

    wip

---
+++ AutofixtureExtender/DataAccessHelper.cs
@@ -0,0 +1,45 @@
+﻿namespace AutofixtureExtender
+{
+    using AutoFixture;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class DataAccessHelper
+    {
+        public static T CreateEf<T>(this IFixture fixture, DbContext context)
+        {
+            var dict = context.Model.GetEntityTypes()
+                 .ToDictionary(g => g.ClrType, g => g);
+
+            return (T)fixture.CreateEf(typeof(T), dict);
+        }
+
+        public static object CreateEf(this IFixture fixture, Type type, Dictionary<Type, IEntityType> dict)
+        {
+            var entityType = dict[type];
+
+            var fixtureGenericMethod = typeof(DataAccessHelper)
+                        .GetMethods(BindingFlags.Static | BindingFlags.NonPublic)
+                        .Where(mi => mi.Name == nameof(_create) && mi.IsGenericMethod)
+                        .Single()
+                        .MakeGenericMethod(entityType.ClrType);
+
+            object obj = fixtureGenericMethod.Invoke(null, new object[] { fixture })!;
+            foreach (var foreignKey in entityType.GetForeignKeys())
+            {
+                var navigation = foreignKey.DependentToPrincipal!;
+
+                var navigationObject = fixture.CreateEf(navigation.TargetEntityType.ClrType, dict);
+                navigation.PropertyInfo!.SetValue(obj, navigationObject, null);
+            }
+
+            return obj;
+        }
+
+        static T _create<T>(IFixture fixture) => fixture.Create<T>();
+    }
+}
---
+++ AutofixtureExtender/FixtureExtend.cs
@@ -0,0 +1,62 @@
+﻿namespace AutofixtureExtender
+{
+    using System;
+    using System.Collections.Generic;
+    using AutoFixture;
+    using AutoFixture.Dsl;
+    using AutoFixture.Kernel;
+
+    public class FixtureExtend : Fixture
+    {
+        private readonly Dictionary<Type, List<Func<ISpecimenBuilder, ISpecimenBuilder>>> _specimenBuilders = new Dictionary<Type, List<Func<ISpecimenBuilder, ISpecimenBuilder>>>();
+
+        public FixtureExtend()
+        {
+        }
+
+        public FixtureExtend(DefaultRelays engineParts)
+            : base(engineParts)
+        {
+        }
+
+        public FixtureExtend(ISpecimenBuilder engine, MultipleRelay multiple)
+            : base(engine, multiple)
+        {
+        }
+
+        public void AddCustomization<T>(Func<IPostprocessComposer<T>, ISpecimenBuilder> composerTransformation)
+        {
+            this.Customize(composerTransformation);
+
+            var key = typeof(T);
+            Func<ISpecimenBuilder, ISpecimenBuilder> func = sb => composerTransformation.Invoke((IPostprocessComposer<T>)sb);
+
+            if (!this._specimenBuilders.ContainsKey(key))
+            {
+                this._specimenBuilders[key] = new List<Func<ISpecimenBuilder, ISpecimenBuilder>>();
+            }
+
+            this._specimenBuilders[key].Add(func);
+        }
+
+        public IPostprocessComposer<T> BuildWith<T>()
+        {
+            IPostprocessComposer<T> build = this.Build<T>();
+
+            var builders = this._specimenBuilders.GetValueOrDefault(typeof(T), new List<Func<ISpecimenBuilder, ISpecimenBuilder>>());
+            foreach (var specimenBuilder in builders)
+            {
+                build = (IPostprocessComposer<T>)specimenBuilder(build);
+            }
+
+            //var abc = this.Customizations.OfType<IPostprocessComposer<T>>().ToList();
+            //var build1 = this.Build<T>() as CompositeNodeComposer<T>;
+            //CompositeNodeComposer<T> res = (CompositeNodeComposer<T>)build1.Compose(abc);
+
+            //var entities = this.Build<Premise>()
+            //  .With(i => i.PropertyName, "Test name")
+            //  .Without(i => i.IncidentPremisesNavigation);
+            return build;
+        }
+    }
+}
